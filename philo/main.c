6a905d9941a4d6a14ab45ba07774e355dddcedf6
El houssaine Bakchich <ebakchic@e1r13p9.1337.ma>
2023-01-19 00:17:26 +0100

    day 03

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: ebakchic <ebakchic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 19:49:28 by ebakchic          #+#    #+#             */
-/*   Updated: 2023/01/17 22:19:51 by ebakchic         ###   ########.fr       */
+/*   Updated: 2023/01/19 00:13:47 by ebakchic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,21 @@
 void	*ft_routing(void *a)
 {
 	t_inf	*ph;
-	//int	i = 0;
 
 	ph = (t_inf *)a;
 	while (1)
 	{
 		pthread_mutex_lock(&ph->forks[ph->index]);
-		//pthread_mutex_lock(&ph->mutex);
+		pthread_mutex_lock(&ph->mutex);
 		printf("%d is taking a left fork\n", ph->index + 1);
-		//pthread_mutex_unlock(&ph->mutex);
-		printf("=======> %d \n", (ph->index + 1) % ph->nph);
+		pthread_mutex_unlock(&ph->mutex);
 		pthread_mutex_lock(&ph->forks[(ph->index + 1) % ph->nph]);
-		//pthread_mutex_lock(&ph->mutex);
+		pthread_mutex_lock(&ph->mutex);
 		printf("%d is taking a right fork\n", ph->index + 1);
-		//pthread_mutex_unlock(&ph->mutex);
+		pthread_mutex_unlock(&ph->mutex);
 		printf("%d is eating\n", ph->index + 1);
-		//pthread_mutex_lock(&ph->mutex);
-		//pthread_mutex_unlock(&ph->mutex);
+		pthread_mutex_lock(&ph->mutex);
+		pthread_mutex_unlock(&ph->mutex);
 		usleep(10000);
 		pthread_mutex_unlock(&ph->forks[ph->index]);
 		pthread_mutex_unlock(&ph->forks[(ph->index + 1) % ph->nph]);
@@ -40,12 +38,12 @@ void	*ft_routing(void *a)
 	return (0);
 }
 
-void    ft_fill(t_list *s, char **av)
+void	ft_fill(t_list *s, char **av)
 {
-	int	i;
-	pthread_mutex_t *forks;
-	pthread_mutex_t mutex;
-	int		index;
+	int				i;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	mutex;
+	int				index;
 
 	i = 0;
 	s->ph = malloc(ft_atoi(av[1]) * sizeof(t_inf));
@@ -57,30 +55,25 @@ void    ft_fill(t_list *s, char **av)
 		pthread_mutex_init(&forks[i], NULL);
 		i++;
 	}
-	
 	i = 0;
 	while (i < index)
 	{
-		s->ph[i].forks =  forks;
+		s->ph[i].forks = forks;
 		s->ph[i].index = i;
-		s->ph[i].nph	= index;
-		s->ph[i].mutex	= mutex;
+		s->ph[i].nph = index;
+		s->ph[i].mutex = mutex;
 		i++;
 	}
 }
 
-int    ft_creat_philo(char **av)
+int	ft_creat_philo(char **av)
 {
-	t_list s;
-	int	i;
+	t_list	s;
+	int		i;
 
-	s.test = 0;
-	s.test2 = 0;
 	ft_fill(&s, av);
 	s.pt = malloc(s.ph[0].nph * sizeof(pthread_t));
-	//s.ph = malloc(s.ph[0].nph * sizeof(t_inf));
 	i = 0;
-	printf("###### %d #######\n", s.ph[0].nph);
 	while (i < s.ph[0].nph)
 	{
 		if (pthread_create(&s.pt[i], NULL, &ft_routing, &s.ph[i]) != 0)
@@ -95,7 +88,6 @@ int    ft_creat_philo(char **av)
 			return (1);
 		i++;
 	}
-	//while (1);
 	free(s.pt);
 	free(s.ph);
 	return (0);
@@ -103,7 +95,7 @@ int    ft_creat_philo(char **av)
 
 int	main(int ac, char **av)
 {
-	int i;
+	int	i;
 
 	if (ft_error(ac, av))
 	{
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: ebakchic <ebakchic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 19:51:40 by ebakchic          #+#    #+#             */
-/*   Updated: 2023/01/17 21:39:42 by ebakchic         ###   ########.fr       */
+/*   Updated: 2023/01/18 23:37:31 by ebakchic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,19 @@
 
 typedef struct t_infop
 {
-	int		nph;
-	int			index;
+	int				nph;
+	int				index;
 	pthread_mutex_t	mutex;
-	pthread_mutex_t *forks;
+	pthread_mutex_t	*forks;
 }	t_inf;
 
 typedef struct t_info
 {
-	int test;
-	int test2;
-
-	t_inf	*ph;
+	t_inf		*ph;
 	pthread_t	*pt;
-	
-}   t_list;
+}	t_list;
 
-int ft_error(int ac, char **av);
+int	ft_error(int ac, char **av);
 int	ft_atoi(const char *str);
 
 #endif
